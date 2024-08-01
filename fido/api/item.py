bd8eeb9e840865f2db9856039d03441d44aa97ad
Srushti238 <srushti@sanskartechnolab.com>
2024-08-01 17:14:51 +0530

    pickup location doctype and item.py

---
+++ fido/api/item.py
@@ -0,0 +1,13 @@
+import frappe
+from frappe import _
+
+@frappe.whitelist()
+def get_item_list(start=0, page_length=20, filters={}, fields=["*"], price_list="Standard Selling", customer=""):
+    try:
+        items = frappe.db.get_list("Item", filters=filters, fields=fields, start=start, page_length=page_length)
+        for item in items:
+            price = frappe.db.get_value("Item Price", filters={"item_code": item.item_code, "customer": customer, "price_list": price_list, "selling": 1}, fieldname="price_list_rate")
+            item["item_price"] = price if price else 0
+        return items
+    except Exception as e:
+        frappe.throw(_("Something went wrong: {0}").format(str(e)))
